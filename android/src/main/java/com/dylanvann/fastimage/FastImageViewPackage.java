1edabd4ed03e6651a1725f3b47c91d51961ee798
a805883237 <805883237@qq.com>
2019-04-24 11:27:47 +0800

    0.45 报错问题

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
@@ -21,4 +21,9 @@ public class FastImageViewPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.<ViewManager>singletonList(new FastImageViewManager());
     }
+
+    // Deprecated RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 }
